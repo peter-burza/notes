a804927deee2a56bffab1b66cca08ec6368110ab
Peter Burza <peterburza48@gmail.com>
2025-07-30 08:30:54 +0000

    Added event listener for enter for submiting forms. Fixing arrow not moving on submit button

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
@@ -6,50 +6,56 @@ import { useState } from "react";
 import Modal from "./Modal";
 
 export default function Login() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [isRegister, setIsRegister] = useState(false)
-  const [isAuthenticating, setIsAuthenticating] = useState(false)
-  const [forgotPassword, setForgotPassword] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isRegister, setIsRegister] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const [forgotPassword, setForgotPassword] = useState(false);
 
-  const { login, signup, sendPassResetEmail } = useAuth()
-  const router = useRouter()
+  const { login, signup, sendPassResetEmail } = useAuth();
+  const router = useRouter();
 
-  const cantAuth = !email.includes('@') || password.length < 6
-  const canSendResetPassEmail = !email.includes('@')
+  const cantAuth = !email.includes("@") || password.length < 6;
+  const canSendResetPassEmail = !email.includes("@");
 
   async function handleAuthUser() {
     // check if email is legit and password is acceptable
     if (cantAuth) {
-      return
+      return;
     }
-    setIsAuthenticating(true)
+    setIsAuthenticating(true);
 
     try {
       if (isRegister) {
         // then we need to register a user
-        await signup(email, password)
+        await signup(email, password);
       } else {
         // otherwise  theyr're wanting to login
-        await login(email, password)
+        await login(email, password);
       }
       // so if we get there with no error - we're authenticated, so push to the notes page
-      router.push('/notes')
+      router.push("/notes");
     } catch (error) {
-      console.log(error.message)
-      // ADD AN ERROR STATE THAT IS CONDITIONALY RENDERED IF THERE IS AN ERROR, AND SHOWS THE ERROR MESSAGE ON SCREEN FOR THE USER 
+      console.log(error.message);
+      // ADD AN ERROR STATE THAT IS CONDITIONALY RENDERED IF THERE IS AN ERROR, AND SHOWS THE ERROR MESSAGE ON SCREEN FOR THE USER
     } finally {
-      setIsAuthenticating(false)
+      setIsAuthenticating(false);
     }
   }
 
   function handleForgottPassword() {
-    sendPassResetEmail(email)
-    alert("If you don't recieve an email, please check your SPAM also.")
+    sendPassResetEmail(email);
+    alert("If you don't recieve an email, please check your SPAM also.");
   }
 
   function handleCloseModal() {
-    setForgotPassword(false)
+    setForgotPassword(false);
+  }
+
+  function handleKeyDown(event) {
+    if (event.key == "Enter") {
+      handleAuthUser();
+    }
   }
 
   return (
@@ -57,13 +63,28 @@ export default function Login() {
       {forgotPassword && (
         <Modal handleCloseModal={handleCloseModal}>
           <h2>Forgot password?</h2>
-          <p>Just type you're email down below and we'll send you an email to reset your password.</p>
+          <p>
+            Just type you're email down below and we'll send you an email to
+            reset your password.
+          </p>
           <div className="forgot-password-email-container">
             <p>Email</p>
-            <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="text" placeholder="Enter your email adress" />
+            <input
+              value={email}
+              onKeyDown={handleKeyDown}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              type="text"
+              placeholder="Enter your email adress"
+            />
           </div>
-          <button onClick={handleForgottPassword} disabled={canSendResetPassEmail} className="submit-btn">
-            <h3>{isAuthenticating ? 'Submitting...' : 'Submit'}</h3>
+          <button
+            onClick={handleForgottPassword}
+            disabled={canSendResetPassEmail}
+            className="submit-btn"
+          >
+            <h3>{isAuthenticating ? "Submitting..." : "Submit"}</h3>
             <h3 className="arrow">-&gt;</h3>
           </button>
         </Modal>
@@ -71,37 +92,69 @@ export default function Login() {
       <div className="login-container">
         <h1 className="text-gradient">ECHOES</h1>
         <h2>Thoughts that gently resonate</h2>
-        <p>Create a personal library of neatly organized and searchable notes and ideas.</p>
+        <p>
+          Create a personal library of neatly organized and searchable notes and
+          ideas.
+        </p>
         <div className="full-line"></div>
-        <h3>{isRegister ? 'Create an account' : 'Sign in'}</h3>
+        <h3>{isRegister ? "Create an account" : "Sign in"}</h3>
         <div>
           <p>Email</p>
-          <input value={email} onChange={(e) => { setEmail(e.target.value) }} type="text" placeholder="Enter your email adress" />
+          <input
+            value={email}
+            onKeyDown={handleKeyDown}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="text"
+            placeholder="Enter your email adress"
+          />
         </div>
         <div>
           <p>Password</p>
-          <input value={password} onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder="*******" />
+          <input
+            value={password}
+            onKeyDown={handleKeyDown}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="*******"
+          />
         </div>
-        <button onClick={handleAuthUser} disabled={cantAuth} className="submit-btn">
-          <h3>{isAuthenticating ? 'Submitting...' : 'Submit'}</h3>
+        <button
+          onClick={handleAuthUser}
+          disabled={cantAuth}
+          className="submit-btn"
+        >
+          <h3>{isAuthenticating ? "Submitting..." : "Submit"}</h3>
           <h3 className="arrow">-&gt;</h3>
         </button>
         <div className="secondary-btns-container">
-          <button onClick={() => {
-            setIsRegister(!isRegister)
-          }} className="card-button-secondary">
-            <small>{isRegister ? 'Sign in' : 'Sign up'}</small>
+          <button
+            onClick={() => {
+              setIsRegister(!isRegister);
+            }}
+            className="card-button-secondary"
+          >
+            <small>{isRegister ? "Sign in" : "Sign up"}</small>
           </button>
-          <button onClick={() => {
-            setForgotPassword(true)
-          }} className="card-button-secondary">
+          <button
+            onClick={() => {
+              setForgotPassword(true);
+            }}
+            className="card-button-secondary"
+          >
             <small>Forgot password?</small>
           </button>
         </div>
         <div className="full-line"></div>
         <footer>
           <a target="_blank" href="https://github.com/peter-burza/echoes">
-            <img alt="pfp" src="https://avatars.githubusercontent.com/u/18643421?v=4" />
+            <img
+              alt="pfp"
+              src="https://avatars.githubusercontent.com/u/18643421?v=4"
+            />
             <h6>@peter-burza</h6>
             <i className="fa-brands fa-github"></i>
           </a>
